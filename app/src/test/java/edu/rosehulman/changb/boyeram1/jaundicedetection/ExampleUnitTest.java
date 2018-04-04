99d84a6736af55b61ebe168784f3527984854a68
Ashlee M Boyer <boyeram1@rose-hulman.edu>
2018-04-04 00:07:05 -0400

    Created Android project with a BasicActivity.

---
+++ app/src/test/java/edu/rosehulman/changb/boyeram1/jaundicedetection/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.rosehulman.changb.boyeram1.jaundicedetection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
