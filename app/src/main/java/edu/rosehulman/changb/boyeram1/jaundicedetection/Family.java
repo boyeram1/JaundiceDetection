a758d1b7090cfe8a65340d748d5c1a52c91aeb69
Ashlee M Boyer <boyeram1@rose-hulman.edu>
2018-04-16 17:03:28 -0400

    Made Family Parcelable for passing from LoginActivity to FamilyActivity.

--- app/src/main/java/edu/rosehulman/changb/boyeram1/jaundicedetection/Family.java
+++ app/src/main/java/edu/rosehulman/changb/boyeram1/jaundicedetection/Family.java
@@ -1,12 +1,15 @@
 package edu.rosehulman.changb.boyeram1.jaundicedetection;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.firebase.database.Exclude;
 
 /**
  * Created by changb on 4/15/2018.
  */
 
-public class Family {
+public class Family implements Parcelable {
 
     private String key;
     private String name;
@@ -15,6 +18,23 @@ public class Family {
     public Family() {
         // Needed for JSON serialization
     }
+
+    protected Family(Parcel in) {
+        key = in.readString();
+        name = in.readString();
+    }
+
+    public static final Creator<Family> CREATOR = new Creator<Family>() {
+        @Override
+        public Family createFromParcel(Parcel parcel) {
+            return new Family(parcel);
+        }
+
+        @Override
+        public Family[] newArray(int i) {
+            return new Family[i];
+        }
+    };
 
     public Family(String name) {
         this.name = name;
@@ -39,5 +59,16 @@ public class Family {
 
     public void setKey(String key) {
         this.key = key;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(key);
+        parcel.writeString(name);
     }
 }
